dd4182db322cbca0490a760ae415563a8455c17f
Jota051 <josemariorojasba3@email.com>
2026-06-12 06:50:35 -0600

    feat: eliminar funcionalidad de customers

--- src/StoreBackend.Infraestructure/AppDbContext.cs
+++ src/StoreBackend.Infraestructure/AppDbContext.cs
@@ -12,7 +12,6 @@ public class AppDbContext : DbContext
 
     public DbSet<Product> Products { get; set; }
     public DbSet<Supplier> Suppliers { get; set; }
-    public DbSet<Customer> Customers { get; set; }
     public DbSet<User> Users { get; set; }
 
     public DbSet<Role> Roles { get; set; }
